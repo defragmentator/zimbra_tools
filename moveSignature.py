15459057f9b05e2a9a929232bd3572e9a859c13d
defragmentator <github@horrify.org>
2019-05-17 15:13:04 +0200

    wyglada ze dziala

--- moveSignature.py
+++ moveSignature.py
@@ -1,4 +1,6 @@
 #!/opt/zimbra/bin/zmpython.27
+# -*- coding: utf-8 -*-
+
 ###https://github.com/jimedler/Zimbra-1/tree/master/ZimbraServer/src/java/com/zimbra/cs/account
 __author__ = ('Marcin Ochab', ('marcin.ochab@gmail.com'))
 
@@ -13,16 +15,14 @@ from com.zimbra.soap.admin.type import CacheEntryType
 from com.zimbra.cs.account.soap import SoapProvisioning
 from com.zimbra.client import ZMailbox
 from com.zimbra.common.soap import SoapFaultException
-
+from com.zimbra.common.service import ServiceException
 import argparse
 import re
 import sys
+import time
 
 def flushCache():
-    mProv = SoapProvisioning()
-    mProv.soapSetURI(ZMailbox.resolveUrl(args.url, True))
-    mProv.soapZimbraAdminAuthenticate()
-    mProv.flushCache(CacheEntryType.account,None)
+    dmProv.flushCache(CacheEntryType.account,None)
 
 def verifyEmail( email ):
     if not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]+$", email):
@@ -36,21 +36,19 @@ def verifyDomain( domain ):
 	raise argparse.ArgumentTypeError(msg)
     return domain
 
-def verifyRegexp( regexp ):
-    try:
-	re.compile(regexp)
-	return regexp
-    except:
-	msg = "%r is not a regexp expression" % regexp
-	raise argparse.ArgumentTypeError(msg)
-
 parser = argparse.ArgumentParser(description='Zimbra signature editor')
-parser.add_argument('inregexp', type=verifyRegexp)
-parser.add_argument('repregexp')
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-m','--mail', help="changes one mail only", type=verifyEmail)
-group.add_argument('-d','--domain', help="changes mails in domain", type=verifyDomain)
-group.add_argument('-a','--all', help="changes all mails", action="store_true")
+group.add_argument('-sm','--source_mail', help="one mail only", type=verifyEmail)
+group.add_argument('-d','--domain', help="all mails in domain", type=verifyDomain)
+group.add_argument('-a','--all', help="all mails", action="store_true")
+
+
+group2 = parser.add_mutually_exclusive_group(required=False)
+group2.add_argument('-o','--overwrite_existing', help="overwrite existing signatures with same name", action="store_true")
+group2.add_argument('-m','--merge_existing', help="merge existing signatures with same name by prefix", action="store_true")
+
+parser.add_argument('-dm','--destination_mail', help="one mail only destination (default: source_mail)", type=verifyEmail)
+
 parser.add_argument('-t','--test',action="store_true",  help="test mode - not altering real data")
 parser.add_argument('-v','--verbose',action="store_true",  help="verbose mode")
 
@@ -62,10 +60,12 @@ parser.add_argument('-dl','--destination_url',  help="zimbra soap url (default: 
 parser.add_argument('-du','--destination_username',  help="username (default: admin)", default= "admin")
 parser.add_argument('-dp','--destination_password',  help="password", required=False)
 
+#zastępowanie
+#merge z inna nazwa
+
 args = parser.parse_args()
 
-
-def modifySignature( signature, regex, sub ):
+def copySignature( signature, dMail, sMail ):
     content = iter(signature.getContents()).next()
     sigMap = {}
     type = "";
@@ -73,19 +73,36 @@ def modifySignature( signature, regex, sub ):
 	type = "zimbraPrefMailSignatureHTML"
     else:
 	type = "zimbraPrefMailSignature"
+    sigMap[type] = content.getContent()
 
-    newSignature = regex.sub(sub,content.getContent())
-    if newSignature != content.getContent():
-	print "\t => modified!"
-    else:
-	print ""
+    dUser = dmProv.getAccountByName(dMail)
     if args.verbose:
-    	print "Old: " + content.getContent()
-    	print
-    	print "New: " + newSignature
+	print "\n" + content.getContent()
+    if dUser is None:
+	print "No remote user by that name - not cloned!"
+	return
+    print ""
     if not args.test:
-	sigMap[type] = newSignature
-	user.modifySignature(signature.getId(),sigMap)
+	try:
+	    dUser.createSignature(signature.getLabel(),sigMap)
+	except ServiceException	as e:
+	    if e.getCode() == "account.SIGNATURE_EXISTS":
+		print "Signature already exists!"
+		if args.overwrite_existing:
+		    print "overwriting!"
+		    destSignature=dUser.getSignatureByName(signature.getLabel())
+		    dUser.modifySignature(destSignature.getId(),sigMap)
+		if args.merge_existing:
+		    print "renaming"
+		    prefix=""	
+		    while True:
+			prefix=prefix+"_"
+			if dUser.getSignatureByName(signature.getLabel() + prefix + sMail) is None:
+			    break
+		    print "free name: " + signature.getLabel() + prefix + sMail
+		    dUser.createSignature(signature.getLabel() + prefix + sMail, sigMap)
+	    else:
+		raise e
 
 smProv = SoapProvisioning()
 smProv.soapSetURI(ZMailbox.resolveUrl(args.source_url, True))
@@ -94,7 +111,7 @@ try:
     smProv.soapZimbraAdminAuthenticate()
 except:
     try:
-	print "Source: Unable to use local credentials. Trying to authenticate with username and password..."
+	print "Source: Unable to use local credentials. Trying to authenticate with username and password...",
 	smProv.soapAdminAuthenticate(args.source_username, args.source_password)
 	print "OK"
     except SoapFaultException as e:
@@ -107,19 +124,14 @@ try:
     dmProv.soapZimbraAdminAuthenticate()
 except:
     try:
-	print "Destination: Unable to use local credentials. Trying to authenticate with username and password..."
+	print "Destination: Unable to use local credentials. Trying to authenticate with username and password...",
 	dmProv.soapAdminAuthenticate(args.destination_username, args.destination_password)
 	print "OK"
     except SoapFaultException as e:
 	sys.exit("Destination authorization error: "+e.code)
 
 
-
-prov=smProv
-
 #prov = Provisioning.getInstance()
-regex = re.compile(args.inregexp, re.IGNORECASE)
-sub = args.repregexp
 
 if args.test:
     print "TEST MODE!!! no modification will be performed !\n"
@@ -127,23 +139,34 @@ if args.test:
 users = []
 domain = None
 
-if not (args.mail is None):
-    users.append(prov.getAccountByName(args.mail))
+if not (args.source_mail is None):
+    users.append(smProv.getAccountByName(args.source_mail))
 else:
     if args.domain:
-	util = ProvUtil()
-	domain = util.lookupDomain(args.domain, prov);
-    users =prov.getAllAccounts(domain)
+	domain = ProvUtil().lookupDomain(args.domain, smProv);
+    users =smProv.getAllAccounts(domain)
 
 for user in users:
+	if user is None:
+    	    sys.exit("No such source account!")
 	mail = user.getMail()
 	print "Account: "+mail
 	
-	for signature in user.getAllSignatures():
-	    print "\tPodpis: " + signature.getLabel() + " (" + iter(signature.getContents()).next().getMimeType() + ")",
-	    modifySignature(signature, regex, sub)
-	    if args.verbose:
-		print "\n\n"
+	while True:
+	    try:
+		for signature in user.getAllSignatures():
+		    print "\tPodpis: " + signature.getLabel() + " (" + iter(signature.getContents()).next().getMimeType() + ")",
+		    if args.destination_mail is None:
+			copySignature(signature,user.getMail(),user.getMail())
+		    else:
+			copySignature(signature,args.destination_mail,user.getMail())  
+		    if args.verbose:
+			print "\n\n"
+		break
+	    except ServiceException	as e:
+		print e.getCode() + "retrying in 1 s"
+		time.sleep(1)
+		#service.PROXY_ERROR too  many connections
 
 if not args.test:
     flushCache()
